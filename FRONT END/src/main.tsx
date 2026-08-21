4aaa2df06eaa70e99d524b31849c525da10e5812
mielechris <34699111+mielechris@users.noreply.github.com>
2026-08-21 10:28:15 -0700

    Mount agent factory on intelligence floor

--- FRONT END/src/main.tsx
+++ FRONT END/src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import FactoryPanel from './FactoryPanel.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div
+      style={{
+        minHeight: '100vh',
+        background: '#040506',
+      }}
+    >
+      <div style={{ padding: '32px 32px 0' }}>
+        <FactoryPanel />
+      </div>
+      <App />
+    </div>
   </StrictMode>,
 )
