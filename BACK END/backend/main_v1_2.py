ff2f4d2dbdabf930ae26ffbcd8d6f0dc122993f8
mielechris <34699111+mielechris@users.noreply.github.com>
2026-08-21 10:43:35 -0700

    Start continuous intelligence ingestion with V1.2 app

--- BACK END/backend/main_v1_2.py
+++ BACK END/backend/main_v1_2.py
@@ -1,7 +1,10 @@
 from main_v1_1 import app
 from factory.router import router as factory_router
 from factory.system_agents import ensure_system_agents
+from intelligence.ingestion import ingestion_service
 
 
 ensure_system_agents()
 app.include_router(factory_router)
+app.add_event_handler("startup", ingestion_service.start)
+app.add_event_handler("shutdown", ingestion_service.stop)
