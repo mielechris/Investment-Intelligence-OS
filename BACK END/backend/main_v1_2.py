dc6c7c4612433aa10a64a2eb36eefdd11b39511a
mielechris <34699111+mielechris@users.noreply.github.com>
2026-08-21 10:40:18 -0700

    Seed persistent system agents at V1.2 startup

--- BACK END/backend/main_v1_2.py
+++ BACK END/backend/main_v1_2.py
@@ -1,5 +1,7 @@
 from main_v1_1 import app
 from factory.router import router as factory_router
+from factory.system_agents import ensure_system_agents
 
 
+ensure_system_agents()
 app.include_router(factory_router)
