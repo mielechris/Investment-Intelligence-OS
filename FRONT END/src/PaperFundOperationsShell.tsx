2d30b1b2f046335cf6b90441bed94025ee3e28fe
mielechris <34699111+mielechris@users.noreply.github.com>
2026-08-26 19:50:26 -0700

    Batch 9D: mount Family Network cortex

--- FRONT END/src/PaperFundOperationsShell.tsx
+++ FRONT END/src/PaperFundOperationsShell.tsx
@@ -1,10 +1,12 @@
 import FactoryIntelligenceExperienceShell from "./FactoryIntelligenceExperienceShell";
+import FamilyNetworkCortex from "./FamilyNetworkCortex";
 import PaperFundOperationsDock from "./PaperFundOperationsDock";
 
 export default function PaperFundOperationsShell() {
   return (
     <>
       <FactoryIntelligenceExperienceShell />
+      <FamilyNetworkCortex />
       <PaperFundOperationsDock />
     </>
   );
