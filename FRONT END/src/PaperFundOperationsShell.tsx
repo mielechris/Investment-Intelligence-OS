7c160d674254c6c5e2d82ae5978c5ae8ec984900
mielechris <34699111+mielechris@users.noreply.github.com>
2026-08-27 12:17:50 -0700

    Mount Batch 10B command layer in operations shell

--- FRONT END/src/PaperFundOperationsShell.tsx
+++ FRONT END/src/PaperFundOperationsShell.tsx
@@ -1,9 +1,11 @@
+import Batch10BCommandLayer from "./Batch10BCommandLayer";
 import FactoryIntelligenceExperienceShell from "./FactoryIntelligenceExperienceShell";
 import PaperFundOperationsDock from "./PaperFundOperationsDock";
 
 export default function PaperFundOperationsShell() {
   return (
     <>
+      <Batch10BCommandLayer />
       <FactoryIntelligenceExperienceShell />
       <PaperFundOperationsDock />
     </>
